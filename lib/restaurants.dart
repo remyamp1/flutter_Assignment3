3fd4e0934d6c190dbd20f717433de4c4d9ec9b14
remyamp <remyamp774@gmail.com>
2024-09-12 08:33:40 +0530

    11-9-24

--- lib/restaurants.dart
+++ lib/restaurants.dart
@@ -53,16 +53,77 @@ class Restaurants extends StatelessWidget {
                     physics: NeverScrollableScrollPhysics(),
                     shrinkWrap: true,
                     separatorBuilder: (context, index) {
-                      return SizedBox(height: 25);
+                      return SizedBox(
+                        height: 25);
                       
                     },
-                    itemCount: 4,
-                    itemBuilder:(context,index){
-                      return Container(
-                        height: 100,
-                        decoration: BoxDecoration
-                        (borderRadius: BorderRadius.circular(15),color: Colors.white),
-                      );
+                itemCount:3,
+               itemBuilder:(context,index){
+                return Container(
+                  height: 150,
+                   decoration: BoxDecoration(
+                               borderRadius: BorderRadius.circular(15),
+                               color: const Color.fromARGB(255, 102, 101, 101),
+                               ),
+
+                                child: Padding(
+               padding: const EdgeInsets.all(12.0),
+               child: Row(
+               children: [
+                 Container(
+                height: 125,
+                width: 110,             
+                decoration: BoxDecoration(borderRadius:BorderRadius.circular(25),
+                image: DecorationImage(image: AssetImage(Database.MyList[index]["image"]),
+                fit: BoxFit.cover,
+                ),
+                
+                ),),
+                 SizedBox(width: 10),
+                                                     
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                    SizedBox(height: 5),
+               Text(
+                Database.MyList[index]["name"],
+                style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold, 
+                color: Colors.white,
+                   
+                ),
+                ),
+                
+                SizedBox(height: 5),
+                    Row(children: [
+                       Icon(Icons.currency_rupee,
+                       color: Colors.white,
+                       ),                                     
+                         Text(
+                          Icon(Icons.map,color: Colors.white),
+                          Database.MyList[index]["name1"],
+                          style: TextStyle(fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+               
+                      ),            
+                   
+                   ],                              
+               ),
+               SizedBox(height: 19,),
+               Row(
+                children: [
+                  Icon(Icons.star,color: Colors.yellow,),
+                  Text(Database.MyList[index]["text"],
+                  style: TextStyle(color: Colors.white),),
+                ],
+               ),
+                   ],                               
+                  ),    
+
+                             
+        
                       
                     
                       
@@ -71,14 +132,13 @@ class Restaurants extends StatelessWidget {
                       
                       
                     
-                    },
+                    
                 
-                  ),
-
+                   
              
 
-        
-
+               
+      );},),
             
              ],
               
